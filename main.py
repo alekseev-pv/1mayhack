e3cfe75991ac3e430053ba68e6e5fc640f6e3326
Vlad Gagai <gagai.vlad@gmail.com>
2022-05-01 22:09:01 +0300

    added definitions, pronunciations, translations changed

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import re
 import requests
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, Filters, MessageHandler
 from dotenv import load_dotenv
+import pprint as p
 
 
 load_dotenv()
-
 TG_TOKEN = os.getenv('TG_TOKEN')
 APP_ID = os.getenv('APP_ID')
 APP_KEY = os.getenv('APP_KEY')
@@ -37,10 +37,9 @@ def get_word_language(word):
     return 'en'
 
 
-def get_translations(word_id, language):
+def get_translations(word_id, source_lang_translate):
     """Получает список переводов."""
-    source_lang_translate = language
-    if language == 'ru':
+    if source_lang_translate == 'ru':
         target_lang_translate = 'en'
     else:
         target_lang_translate = 'ru'
@@ -53,21 +52,81 @@ def get_translations(word_id, language):
     return translations
 
 
-def translate(update, context):
+def get_translation_message(translations):
+    message = 'Переводы:\n\n'
+
+    if len(translations) == 1:
+        message = 'Перевод:\n\n'
+        for translation in translations:
+            translation = translation.get('text')
+            message += f'1) {translation};\n'
+        return message
+
+    i = 1
+    for translation in translations:
+        translation = translation.get('text')
+        message += f'{i}) {translation};\n'
+        i += 1
+    return message
+
+
+def get_info(update, context):
     """Запускает остальные функции и отправляет сообщение с переводом или переводами в чат."""
     chat = update.effective_chat
 
     word = get_word(update)
     lang = get_word_language(word)
-    translations = get_translations(word_id=word, language=lang)
+    translations = get_translations(word_id=word, source_lang_translate=lang)
+    info = get_word_info(word, lang)
+    definitions = get_word_definitions(info)
 
-    for translation in translations:
-        translation = translation.get('text')
-        context.bot.send_message(chat_id=chat.id, text=translation)
+    translation_message = get_translation_message(translations)
+    definition_message = get_definition_message(definitions)
+    audio = get_pronunciations(info)
+
+    context.bot.send_message(chat_id=chat.id, text=translation_message)
+    context.bot.send_message(chat_id=chat.id, text=definition_message)
+    context.bot.send_audio(chat_id=chat.id, audio=audio)
+
+
+def get_word_info(word_id, source_lang):
+    if source_lang == 'en':
+        url = f'{ENDPOINT}/entries/{source_lang}/{word_id}'
+        request = requests.get(url=url, headers=HEADERS)
+        info = request.json().get('results')[0].get('lexicalEntries')[0].get('entries')[0]
+    return(info)
+
+
+def get_pronunciations(info):
+    pronunciations = info.get('pronunciations')[0].get('audioFile')
+    return pronunciations
+
+
+def get_word_definitions(info):
+    subsenses = info.get('senses')[0].get('subsenses')
+    definitions = []
+    for subsense in subsenses:
+        definitions += subsense.get('definitions')
+    return definitions
+
+
+def get_definition_message(definitions):
+    message = 'Определения:\n\n'
+    if len(definitions) == 1:
+        message = 'Определения:\n\n'
+        for definition in definitions:
+            message += f'1) {definition};\n'
+        return message
+
+    i = 1
+    for definition in definitions:
+        message += f'{i}) {definition};\n'
+        i += 1
+    return message
 
 
 def main():
-    UPDATER.dispatcher.add_handler(MessageHandler(Filters.text, translate))
+    UPDATER.dispatcher.add_handler(MessageHandler(Filters.text, get_info))
 
     UPDATER.start_polling(poll_interval=0.5)
     UPDATER.idle
