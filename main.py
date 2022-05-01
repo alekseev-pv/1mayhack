3c419fa6ed979c285c33b12b64621b754422acea
Валерий Юрченко <valerayurchenko14@yandex.ru>
2022-05-01 22:21:54 +0500

    first release

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from telegram import Bot
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+updater = Updater(token=TELEGRAM_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
+URL = 'http://rutor.info'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def wake_up(update, context):
+    chat = update.effective_chat
+    message = 'Я помогу найти торрент с rutor! Напиши мне поисковый запрос'
+    context.bot.send_message(chat_id=chat.id, text=message)
 
 
-# Press the green button in the gutter to run the script.
+def search(update, context):
+    chat = update.effective_chat
+    text = update['message']['text']
+    search_url = f'{URL}/search/{text}'
+    request = requests.get(search_url)
+    soup = BeautifulSoup(request.text, 'lxml')
+    link_list = soup.find('div', id='index').find_all('tr', limit=11)
+    for torrent in link_list:
+        links = torrent.select('a[href*="/torrent"]')
+        name = torrent.text
+        downloads = torrent.find('a', class_='downgif')
+        for link in links:
+            link = link.get('href')
+            download = downloads.get('href')
+            message = (
+                f'Название файла: {name} '
+                f'Ссылка на файл: {URL}{link} '
+                f'Ссылка на скачку: {download} '
+            )
+            context.bot.send_message(chat_id=chat.id, text=message)
+
+
 if __name__ == '__main__':
-    print_hi('Initial 1mayhack')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, search))
+    updater.start_polling()
+    updater.idle()
---
+++ readme.md
@@ -0,0 +1,41 @@
+# RUTOR_BOT
+## Бот-поисковик .torrent файлов для rutor.info
+## Запланированные возможности:
+- Поиск .torrent файлов
+- Отправка пользователю .torrent - файла по запросу
+- Просмотр дополнительных сведений о файле
+- Возможность использования magnet-ссылок
+- Интеграция с торрент-клиентом Transmission
+## На 01.05.2022 бот умеет:
+- Искать .torrent - файлы
+- Просмотр дополнительных сведений о файле
+
+> При размещении бота на территории РФ используйте VPN (временно)
+> Не забудьте вписать токен в константу TELEGRAM_TOKEN
+## Tech
+
+Проект rutor_bot использует следующие технологии:
+
+- APScheduler           3.6.3
+- backports.zoneinfo    0.2.1
+- beautifulsoup4        4.11.1
+- bs4                   0.0.1
+- cachetools            4.2.2
+- certifi               2021.10.8
+- charset-normalizer    2.0.12
+- idna                  3.3
+- lxml                  4.8.0
+- pip                   20.1.1
+- python-telegram-bot   13.11
+- pytz                  2022.1
+- pytz-deprecation-shim 0.1.0.post0
+- requests              2.27.1
+- setuptools            47.1.0
+- six                   1.16.0
+- soupsieve             2.3.2.post1
+- tornado               6.1
+- tzdata                2022.1
+- tzlocal               4.2
+- urllib3               1.26.9
+
+**Поднять паруса! Хо-Хо**
