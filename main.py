0af823dc25f234491072c7e89233d0c5f8085cf5
Заборовский Денис <zrivkoren@gmail.com>
2022-05-01 19:37:59 +0800

    бот умеет отправлять сообщения в ответ на нажатие кнопки и старт бота

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram import ReplyKeyboardMarkup
+
 import logging
 import requests
 
@@ -11,11 +13,10 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-ENDPOIT_YANDEX_IMAGE = 'https://yandex.ru/images/search?source=collections' \
+URL_YANDEX_IMAGE = 'https://yandex.ru/images/search?source=collections' \
                       '&rpt=imageview&url=urltofile&'
 
 
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def send_message(bot, message):
@@ -26,6 +27,35 @@ def send_message(bot, message):
     except Exception as error:
         logging.error(f'Сбой в работе бота телеги: {error}')
 
+def get_new_text():
+    return f'Просто новый текст'
+
+def new_text(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, get_new_text())
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/newtext']], resize_keyboard=True)
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=f'Привет, {name}. Посмотри на текст, которого я для тебя нашел',
+        reply_markup=button
+    )
+
+def main():
+    updater = Updater(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newtext', new_text))
+    updater.start_polling()
+    updater.idle()
+
+
 if __name__ == '__main__':
-    send_message(bot, f'Отправка первого сообщения из программы')
+    main()
 
